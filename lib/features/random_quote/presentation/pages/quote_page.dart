34a12dce28bae9ff9fdd005d1524fbf5dffa424b
AbdullahShweikani <abdullahshweikanii@gmail.com>
2022-09-14 09:52:36 +0300

    commint

--- lib/features/random_quote/presentation/pages/quote_page.dart
+++ lib/features/random_quote/presentation/pages/quote_page.dart
@@ -12,9 +12,10 @@ class QuotePage extends StatelessWidget {
       ),
       body: InkWell(
           onTap: () {
-           Constants.showToast(context: context, message: "hi Abdullah");
+            Constants.showToast(context: context, message: "hi Abdullah");
           },
           child: const Center(child: Text('the main page'))),
     );
   }
 }
+//lfjijdfijfdi
